ac9b42a24ee294a9b3fec0e250742b8b42e1dda6
sapaeff <sapaeff@yandex.team.ru>
2020-05-03 20:02:58 +0300

    more tests

---
+++ dvm_sys/examples/CheckPointTests/test_checkpoint_exit_and_load_parallel_3d.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <assert.h>
+#include "all_header.h"
+
+const char *name = "test_checkpoint_exit_and_load_parallel";
+enum Mode mode = PARALLEL;
+
+void run(int val) {
+    #pragma dvm array distribute[block][block][block]
+    unsigned int A[N][N][N];
+
+    #pragma dvm parallel([i][j][k] on A[i][j][k]) cuda_block(256)
+    for(int i = 0; i < N; ++i) {
+        for(int j = 0; j < N; ++j) {
+            for(int k = 0; k < N; ++k) {
+                A[i][j][k] = 0x10000 * i + 0x100 * j + 0x1 * k + val;
+            }
+        }
+    }
+
+    DvmType **dvmDesc = (DvmType **) malloc(1 * sizeof(DvmType *));
+    dvmDesc[0] = A;
+    dvmh_create_or_bind_control_point(name, dvmDesc, 1, nfiles, mode);
+    dvmh_load_control_point(name);
+
+    #pragma dvm parallel([i][j][k] on A[i][j][k]) cuda_block(256)
+    for(int i = 0; i < N; ++i) {
+        for(int j = 0; j < N; ++j) {
+            for(int k = 0; k < N; ++k) {
+                assert(A[i][j][k] == 0x10000 * i + 0x100 * j + 0x1 * k);
+            }
+        }
+    }
+
+    dvmh_save_control_point(name);
+    dvmh_deactivate_control_point(name);
+}
+
+int main()
+{
+    run(0);
+    run(1);
+    run(2);
+    return 0;
+}
