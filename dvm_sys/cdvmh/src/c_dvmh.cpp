c8dd2421c1940c912acc3c8b0a6ab1a24870770d
sapaeff <sapaeff@yandex.team.ru>
2020-11-23 12:43:49 +0300

    save changes (doesnt work)

--- dvm_sys/cdvmh/src/c_dvmh.cpp
+++ dvm_sys/cdvmh/src/c_dvmh.cpp
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]) {
         usage(stderr);
         return 1;
     }
-    logLevel = ERROR;
+    logLevel = TRACE;
     const char *sett = getenv("DVMH_LOGLEVEL");
     if (sett) {
         logLevel = (LogLevel)atoi(sett);
